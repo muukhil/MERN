bfef7a9a1b267b96f043ce030e75fd9a242251f9
muukhil <144228537+muukhil@users.noreply.github.com>
2025-02-17 08:35:57 +0530

    JS FN

---
+++ JavaScript/javasc.js
@@ -0,0 +1,29 @@
+// const a=10
+// const b="abc"
+// const c=true
+// console.log(typeof(a),typeof(b),typeof(c))
+//object1={
+//     "firstname":"St Joseph",
+//     "Lastname":"Institute of Technology",
+//     "depts":["CSE","IT","ADS","ECE"],
+//     "training":"MERN"
+// }
+// console.log(object1)
+// // key may or maynot have quotes
+// console.log(object1.depts) // to print a specific key-value
+// console.log(object1['training'])
+// object2={}
+// object2["Firstname"]="Hello"
+// object2["Lastname"]="SJIT"
+// object2["training"]=["GIT","NODE","Mongodb","HTML","CSS","EXPRESS"]
+// console.log(object2)
+// object3=new Object()
+// object3.training="MERN"
+// object3.depts=["ADS","CSE","ECE","IT"]
+// console.log(object3)
+//SET
+// set=new Set("hello");
+// console.log(set);//seperates as character
+// set=new Set(["Hello"])
+// //the hello can be added using set.add("hello")
+// console.log(set)// print as a string
