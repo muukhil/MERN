805443f8b720233cd4b975d7066c7f6468b26625
muukhil <144228537+muukhil@users.noreply.github.com>
2025-02-19 09:26:16 +0530

    (18.2.25) AN

---
+++ sjitFrontend/src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Home from './assets/Components/Functional_Components/Home'
+import About from './assets/Components/Functional_Components/About'
+import Gallery from './assets/Components/Functional_Components/Gallery'
+import Signup from './assets/Components/Functional_Components/Signup'
+import Contact from './assets/Components/Functional_Components/Contact'
+
+function App() {
+
+  return (
+    <div>
+      <hr />
+      <Home />
+      <hr />
+      <About />
+      <hr />
+      <Gallery />
+      <hr />
+      <Signup />
+      <hr />
+      <Contact />
+    </div>
+  )
+}
+
+export default App
